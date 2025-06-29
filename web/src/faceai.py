a6d666b6e078c778b3719809ae2f6a839cca319d
norino0620 <norino4788@gmail.com>
2025-06-30 02:04:55 +0900

    AI結合済み

--- web/src/faceai.py
+++ web/src/faceai.py
@@ -10,10 +10,19 @@ def faceSearch(file_path):
     api_key = os.getenv("AI_API_KEY")
 
     client = genai.Client(api_key=api_key)
-    
-    image_path = Path(file_path)
-    uploaded_file = client.files.upload(file=image_path)
 
+    image_path = Path(file_path)
+    base_dir = Path(__file__).resolve().parent  # ← /web/src/
+    image_path = base_dir / image_path 
+    print(f"✅ 実際に使うパス: {image_path}")
+    
+    
+        # ファイルが存在するかチェック（デバッグ用）
+    if not image_path.exists():
+        print("❌ ファイルが存在しません")
+        return
+
+    uploaded_file = client.files.upload(file=image_path)
     response = client.models.generate_content(
         model="gemini-2.5-pro-preview-06-05",
         contents = [
@@ -33,17 +42,18 @@ def faceSearch(file_path):
             - R_forehead = W_forehead / W_cheek
             3. Classify using thresholds:
             - ホームベース型: R_face ≥ 1.178 and R_jaw ≥ 0.95
-            - 逆三角形型: R_forehead ≥ 1.05 and R_jaw ≤ 0.80
+            - 逆三角形: R_forehead ≥ 1.05 and R_jaw ≤ 0.80
             - 面長: 1.0537 ≤ R_face < 1.111
             - 丸顔: 1.112 ≤ R_face ≤ 1.176 and 0.85 ≤ R_jaw ≤ 0.95 and 0.95 ≤ R_forehead ≤ 1.05
             - 卵型: 1.112 ≤ R_face ≤ 1.176 and R_jaw ≤ 0.85 and R_forehead ≤ 1.05
 
-            faceShapeList = ["丸顔", "面長", "ホームベース型", "卵型", "逆三角形型"]
+            faceShapeList = ["丸顔", "面長", "ホームベース型", "卵型", "逆三角形"]
             Answer only one Japanese label from the list, with no additional text.
             """
             ]
         )
     print(response.text)
-
+    return response.text
 if __name__=="__main__":
     main()
+
